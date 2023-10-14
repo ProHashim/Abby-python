355bf75b65b2ade7237ba30feda126360ab202b1
prohashim <prohashim4e@gmail.com>
2023-10-14 23:03:40 +0500

    Class 1 done

---
+++ class1/class1.py
@@ -0,0 +1,31 @@
+#  Ctrl + / to write a comment
+
+# This is a comment
+
+# shift + 3 to write a comment
+
+# Variable
+# We use variables if we want to store some data to use it later and we can use it over and over again.
+# Definition: named memory location is called a variable
+
+# Ok so Memory location:
+    # Memory- storage of computer
+    # Location - place where the data will be stored.
+    #Named - A name give to a memeory location.
+
+# You can assign any name to a variable and that will be called an identifier, but there are some rules to keep in mind.
+
+# How do we write a variable?
+
+name = "Abby"
+
+# How do we run a Python program?
+
+# print ()
+
+print("Hello this is abby")
+print(435453)
+# Python will treat these numbers as if they are simple english sentences
+print('I am also a string and I can write 124444444')
+
+
