4320d9298ddfab71009d569170f12e9cb4dc2f23
prohashim <prohashim4e@gmail.com>
2023-10-28 23:21:38 +0500

    class 2

---
+++ class2/app.py
@@ -0,0 +1,62 @@
+# What are the rules to write variable names (Identi
+hello = "goodbye"
+
+# Writing variables the 9 rules.
+
+# 1 Rule:
+# The variable name must start with a letter or an underscore (_)
+# age = 56 OR _age = 45 (Both of these are valid variable names because they start with a letter OR an _)
+car = "Camry"
+_laptop = "Dell"
+_water = "aqua"
+
+
+# 2 Rule: Identfiers cannot start with a number.
+# 112hello = "value" (NOT valid bc it starts with a number.)
+# 9ty = "value" It will generate a syntax Error.
+_123 = "number"
+# 234 = "wrong"
+
+# 3 Rule: Only can contain the alpha numeric characters
+amazon11 = "Daraz"
+hello900 = "greeting"
+
+# 4 Rule: Identifiers are case-sensative
+abc = "hi"
+Abc = "Helllooo"
+# Both are different
+ABC = 34
+aBC = 23
+Efg = 25
+efg = 76
+
+# 5 Rule: Keywords cannot be used as the name of the variables.
+# keywords are the words that has a pre-defined meaning.
+# for = 4555 Syntax error because we cannot use keywords as the variable names
+
+
+#6 Rule: We cannot use spaces between the variable names if the variable is more than a single word
+# your car = "Toyota"  (Invalid, No spaces allowed)
+# my home = "city"
+
+#7 Rule: cannot contain only digits
+# 453245 = "value" wrong because these are digits only and it will give us Syntax error.
+# 78909 = "incorrect"
+
+#8 Rule: There is no limit on the lenght of the variable name.It can be a single character or it can 100s of characters
+s = "Hiiii"
+asdfahsdkjhfaklsjdhfklajhsdjkhfalksdhlfjahsdljhfajklshdlfahsdlkhfalkhsdlkfhaldshfladhsfjadslhfaldsjhfa = 56
+
+#9 Rule: We can write numbers in between the letters or at the end of the letters
+amazon11amazon = "Walmart" # valid
+a11y = 34 #valid
+a1 = 453246
+
+greetings = "Hi there, I am your new instructor"
+age = "455" # Number can be inside of quotation marks but that will change the data type of it meaning the code will treat as if it is normal english words.
+
+# We dont need quotation marks for writing numbers as values
+height = 170
+
+# we also dont need quotation marks for variable names
+print(height)
