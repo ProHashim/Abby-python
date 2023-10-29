3e99364fde8889889670b05756c7ceec33dc347c
prohashim <prohashim4e@gmail.com>
2023-10-29 23:03:48 +0500

    class 3

---
+++ class3/app.py
@@ -0,0 +1,76 @@
+dummy_data = 45
+print(dummy_data)
+
+# Data type
+# The type of value a variable can hold, e.g numbers or letters etc.
+
+# 1 String
+# Sequence of characters enclosed in quotes.
+greeting_message = "Hello good morning there!"
+hello = 'hi there'
+b = "how are you?"
+print(b)
+
+# multi lines string
+multi = ''' Welcom to triple quotes
+here is a line
+another line
+sure 
+are you?
+ '''
+# it will print the first apostrophe, this is why we can write upto triple quotes only
+a = ''''hello '''
+print(a)
+print(hello)
+# 2 Integer
+# Whole numbers
+age = 67
+temprature = -4
+day = 29
+
+
+# 3 Float -> (Floating point numbers):
+# Any number that consists of a decimal point
+height = 1.7
+length = 7.5
+
+
+# 4 Boolean:
+# it will have the value of either true or false
+
+is_cloudy = True
+abbys_birthday = False
+really_cold = True
+is_1 = False
+present = False
+
+# Good naming conventions
+# meaningful
+# use lowercase letters
+# Be descriptive and avoid single words
+# Follow the PEP8 rules
+
+full_name = "Ahmad Shah"
+current_date = 21
+
+aword = "Hello hiiiiii, "
+description = "yes sure I am here"
+
+print(aword + description)
+cat = "animal 1"
+dog = "animal 2"
+print(cat + dog)
+
+# Errors 
+# If you have made some kind of mistakes in the code it will give you an error
+
+#Type error
+# print(3 + aword)
+agess = 45
+sun = hello
+# print(12 + sun)
+print(agess + 45)
+
+#2 Syntax Error
+# car color = "green"
+# house date = 1989
